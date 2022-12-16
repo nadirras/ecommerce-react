a60466801cecb79cd911416b6025dc2a3cf4030d
nadirras <75488956+nadirras@users.noreply.github.com>
2022-12-17 04:54:08 +0700

    Update ContactUs.js

--- src/Views/ContactUs.js
+++ src/Views/ContactUs.js
@@ -3,7 +3,7 @@ import React from "react";
 function ContactUs(){
     return (
         <div className="font-bold text-2xl mb-3">
-            <a href="#"> Instagram </a>
+            Instagram: @
         </div>)
 }
 
