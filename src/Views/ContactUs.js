0326d64fb73b74dd2eba1e44778ac380fdba1879
nadirras <75488956+nadirras@users.noreply.github.com>
2022-12-17 04:53:06 +0700

    Update ContactUs.js

--- src/Views/ContactUs.js
+++ src/Views/ContactUs.js
@@ -3,7 +3,7 @@ import React from "react";
 function ContactUs(){
     return (
         <div className="font-bold text-2xl mb-3">
-            Twitter: @zuraxchi
+            <a href=#> Instagram </a>
         </div>)
 }
 
